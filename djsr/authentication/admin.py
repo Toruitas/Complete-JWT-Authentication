58f3fe841b9b7f8065f013d5152299d17cdc9b45
Stuart Leitch <toruitas@gmail.com>
2019-06-13 16:28:47 +0800

    Change admin.py for CustomUser

--- djsr/authentication/admin.py
+++ djsr/authentication/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser, UserAdmin)
+
+class CustomUserAdmin(admin.ModelAdmin):
+    model = CustomUser
+
+admin.site.register(CustomUser, CustomUserAdmin)
