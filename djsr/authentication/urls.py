f2ac6e244c37f7d538ca499999d75dedd4ee67dc
Stuart Leitch <toruitas@gmail.com>
2019-06-13 20:15:09 +0800

    1-2e done

--- djsr/authentication/urls.py
+++ djsr/authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-from .views import ObtainTokenPairWithColorView
+from .views import ObtainTokenPairWithColorView, CustomUserCreate
 
 urlpatterns = [
+    path('user/create/', CustomUserCreate.as_view(), name="create_user"),
     path('token/obtain/', ObtainTokenPairWithColorView.as_view(), name='token_create'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- djsr/authentication/views.py
+++ djsr/authentication/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
-
+from rest_framework import status, permissions
+from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import MyTokenObtainPairSerializer
+from rest_framework.views import APIView
+
+from .serializers import MyTokenObtainPairSerializer, CustomUserSerializer
 
 
 class ObtainTokenPairWithColorView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+class CustomUserCreate(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format='json'):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
