e66a980dcd2ba1c13c6a0911bb1c96beb670d648
Stuart Leitch <toruitas@gmail.com>
2019-06-14 10:00:20 +0800

    Part 1 finished.

--- djsr/authentication/urls.py
+++ djsr/authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-from .views import ObtainTokenPairWithColorView, CustomUserCreate
+from .views import ObtainTokenPairWithColorView, CustomUserCreate, HelloWorldView
 
 urlpatterns = [
     path('user/create/', CustomUserCreate.as_view(), name="create_user"),
     path('token/obtain/', ObtainTokenPairWithColorView.as_view(), name='token_create'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('hello/', HelloWorldView.as_view(), name='hello_world')
 ]
--- djsr/authentication/views.py
+++ djsr/authentication/views.py
@@ -21,3 +21,9 @@ class CustomUserCreate(APIView):
                 json = serializer.data
                 return Response(json, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class HelloWorldView(APIView):
+
+    def get(self, request):
+        return Response(data={"hello":"world"}, status=status.HTTP_200_OK)
