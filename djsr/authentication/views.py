fad05bfb53750f23a34a9599d8600a49f523f829
Stuart Leitch <toruitas@gmail.com>
2020-02-19 14:35:45 +0000

    Additional testing

--- djsr/authentication/views.py
+++ djsr/authentication/views.py
@@ -3,8 +3,6 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, BlacklistedToken
-
 from .serializers import MyTokenObtainPairSerializer, CustomUserSerializer
 
 
